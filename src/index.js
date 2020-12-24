d2c330ac2de0a464bf5828d73b10f21b89b12823
Maksim Marchukov <mar.maksim63@gmail.com>
2020-12-24 18:17:09 +0400

    Initial docker

--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ const bot = require('./bot');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/gphoto-bot', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
+mongoose.connect('mongodb://mongo:27017/gphoto-bot', {
+  useNewUrlParser: true
+}).then(() => console.log('DB connected'))
+  .catch(() => console.log('DB was not connected'));
 
 app.get('/auth/google/callback', (req, res) => {
   // { secret_code: req.query.code }
   res.sendFile('./view/index.html', { root: __dirname });
 });
 
-app.listen(8080, () => {
-  console.log(`App listening!`);
+app.listen(3000, () => {
+  console.log(`App listening!!!!!`);
 })
