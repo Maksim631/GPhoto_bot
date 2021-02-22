1f29617a3c4afd92bd3c7b45c12c24a90bccc431
Maksim Marchukov <mar.maksim63@gmail.com>
2021-02-22 22:39:19 +0400

    Complete refactoring

--- src/config.js
+++ src/config.js
@@ -1,29 +1,30 @@
-export default config = {}
+const config = {
+  // The OAuth client ID from the Google Developers console.
+  oAuthClientID:
+    '127766624439-5dbt43cr0rhp5lvjq7a19btdhutd19k0.apps.googleusercontent.com',
 
-// The OAuth client ID from the Google Developers console.
-config.oAuthClientID =
-  '127766624439-5dbt43cr0rhp5lvjq7a19btdhutd19k0.apps.googleusercontent.com'
+  // The OAuth client secret from the Google Developers console.
+  oAuthclientSecret: 'uWDx3NN3Ry_6Ib201dhil-lT',
 
-// The OAuth client secret from the Google Developers console.
-config.oAuthclientSecret = 'uWDx3NN3Ry_6Ib201dhil-lT'
+  // The callback to use for OAuth requests. This is the URL where the app is
+  // running. For testing and running it locally, use 127.0.0.1.
+  oAuthCallbackUrl: 'https://max.andrewsha.net/auth/google/callback',
+  
+  // The scopes to request. The app requires the photoslibrary.readonly and
+  // plus.me scopes.
+  scopes: [
+    'https://www.googleapis.com/auth/photoslibrary.appendonly',
+    'profile',
+  ],
 
-// The callback to use for OAuth requests. This is the URL where the app is
-// running. For testing and running it locally, use 127.0.0.1.
-config.oAuthCallbackUrl = 'https://max.andrewsha.net/auth/google/callback'
+  // The API end point to use. Do not change.
+  apiEndpoint: 'https://photoslibrary.googleapis.com',
 
-// The port where the app should listen for requests.
-config.port = 8080
+  tgToken: '1305964524:AAHVgkCtEKnR9swVbgqyxhxb1W1yh4giuFQ',
 
-// The scopes to request. The app requires the photoslibrary.readonly and
-// plus.me scopes.
-config.scopes = [
-  'https://www.googleapis.com/auth/photoslibrary.appendonly',
-  'profile',
-]
+  // The port where the app should listen for requests.
 
-// The API end point to use. Do not change.
-config.apiEndpoint = 'https://photoslibrary.googleapis.com'
+  port: 3000,
+}
 
-config.tgToken = '1305964524:AAHVgkCtEKnR9swVbgqyxhxb1W1yh4giuFQ'
-
-config.port = 3000
+export default config
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import { MongoClient } from 'mongodb'
-import Bot from './bot/bot.js'
-import config from './config'
-import UserDao from './db/user.dao'
-import app from './server'
+import mongodb from 'mongodb'
+import isBotCreated from './bot/bot.js'
+import config from './config.js'
+import UserDao from './db/user.dao.js'
+import app from './server.js'
 
-new Bot(logger.child({ class: 'TelegramBot' }))
+if (isBotCreated) {
+  console.log('Bot successfully connected')
+} else {
+  console.log("Bot couldn't connect")
+}
 
+const { MongoClient } = mongodb
 MongoClient.connect('mongodb://mongo:27017/gphoto-bot', {
   useNewUrlParser: true,
 })
