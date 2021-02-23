fbabd423b3019e7602c8373ab8c3770f399fc2d3
Maksim Marchukov <mar.maksim63@gmail.com>
2021-02-23 17:18:46 +0400

    return real server

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {
 
   // The callback to use for OAuth requests. This is the URL where the app is
   // running. For testing and running it locally, use 127.0.0.1.
-  oAuthCallbackUrl: 'http://127.0.0.1:22000/auth/google/callback',
+  oAuthCallbackUrl: 'https://max.andrewsha.net/auth/google/callback',
   
   // The scopes to request. The app requires the photoslibrary.readonly and
   // plus.me scopes.
